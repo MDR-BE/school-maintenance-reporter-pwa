dafd2b2b3b69852f7a308d20afcd1531aceadd93
MDR-BE <maartenderyck@yahoo.com>
2026-08-22 09:39:09 +0200

    Update API base URL in getApiBaseUrl function

--- js/utils.js
+++ js/utils.js
@@ -8,7 +8,7 @@
 function getApiBaseUrl() {
   // TODO: Replace with the actual web app URL after deployment
   // For now, we'll return a placeholder; the user must update this.
-  return 'https://script.google.com/macros/s/AKfycbw_qjk7BeKUqqPUGyB0-jDU0O4sDhJ_bhl1Cf9MsEJY1dCe8OrpDhv2YI5n9EkX-ZU/exec';
+  return 'https://script.google.com/macros/s/AKfycbzwlJrjv2HvXLOjnT1NKYr1OhpbMLrKFshnQgFwLEuo8GMh4miga_gVThV3Afsdyvw/exec';
 }
 
 /**
